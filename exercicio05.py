d22fe94aa3600d11c7d30725793560a41377ec02
brubruh <00001111113804sp@al.educacao.sp.ov.br>
2025-05-06 12:07:43 -0300

    exercicio 5

--- exercicio05.py
+++ exercicio05.py
@@ -1,11 +1,12 @@
-def cadastro(nome, email, serie):
+def cadastro(nome, email, serie, nota1, nota2, nota3):
     lista = []
     dicionario = {
         'nome' : nome,
         'email' : email,
-        'serie' : serie
+        'serie' : serie,
+        "notas" : [nota1, nota2, nota3]
     }
     lista.append(dicionario)
     return dicionario
 
-print(cadastro("bruno", "brunolourencone12@gmail.com", "2tb"))
+print(cadastro("bruno", "brunolourencone12@gmail.com", "2tb", 10, 9, 5))
