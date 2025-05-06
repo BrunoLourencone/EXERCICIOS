d939907737a2e6d6ff38ca7bafa512aec120a715
brubruh <00001111113804sp@al.educacao.sp.ov.br>
2025-05-06 07:53:02 -0300

    exercicio dois

---
+++ exercicio02.py
@@ -0,0 +1,9 @@
+def listaNumerica(lista):
+    somar = 0
+    for i in lista:
+        somar += i
+    return somar
+
+numeros_ale = [9, 23, 76, 19, 10, 25, 20]
+resultado = listaNumerica(numeros_ale)
+print(resultado)
