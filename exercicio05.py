b78629525a96234ec4aac789b347f67ac3b37366
brubruh <00001111113804sp@al.educacao.sp.ov.br>
2025-05-19 14:25:33 -0300

    exercicio 07

--- exercicio05.py
+++ exercicio05.py
@@ -1,7 +1,17 @@
 from exercicio04 import listaNumerica 
 
-def cadastro(nome, email, serie, nota1, nota2, nota3):
-    lista = []
+lista = []
+
+def obter_dados_aluno():
+    nome = input("informe o nome completo do aluno: ")
+    email = input("informe o email do aluno: ")
+    serie = input("informe a serie do aluno: ")
+    nota1 = int(input("informe a nota1 do aluno: "))
+    nota2 = int(input("informe a nota2 do aluno: "))
+    nota3 = int(input("informe a nota3 do aluno: "))
+    return cadastro(nome, email, serie, nota1, nota2, nota3)
+
+def cadastro(nome, email, serie, nota1=0, nota2=0, nota3=0):
 
     notas = [nota1, nota2, nota3]
 
@@ -13,7 +23,14 @@ def cadastro(nome, email, serie, nota1, nota2, nota3):
         "media" : listaNumerica(notas)
     }
     lista.append(dicionario)
-    media = listaNumerica(dicionario["notas"])
+    
     return dicionario
 
-print (cadastro("bruno", "brunolourencone12@gmail.com", "2TB", 10, 23, 7))
+
+obter_dados_aluno()
+
+def  mostrar_dados_alunos(dados_alunos):
+    for lista in dados_alunos:
+        print(f"nome do aluno: {lista["nome"]}")
+    
+
