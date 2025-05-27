ba35c1d676f7290e2ed58050f1f058f60213d5d7
brubruh <00001111113804sp@al.educacao.sp.ov.br>
2025-05-27 07:40:07 -0300

    exercicio 10

--- exercicio07.py
+++ exercicio07.py
@@ -1,7 +1,9 @@
-db_clientes = "db_clientes.json"
-# clientes = []
 import json
 import os
+
+db_clientes = "db_clientes.json"
+# clientes = []
+
 def carregar_dados():
     if os.path.exists(db_clientes):
         with open(db_clientes, "r", encoding="utf-8") as arq_json:
@@ -11,16 +13,16 @@ def carregar_dados():
 
  
 def obter_dados_cliente():
-    nome_cliente = input("Informe o Nome Completo do Cliente")
-    cpf_cliente = int(input("Informe o CPF Completo do Cliente"))
-    rg_cliente = int(input("Informe o  Numero de RG Completo do Cliente"))
-    data_de_nascimento_cliente = input("Informe a Data de Nascimento do Cliente")
-    endereco_cliente = input("Informe o Endereço do Cliente")
-    cidade_cliente = input("Informe a Cidade do Cliente")
-    estado_cliente = input("Informe o Estado do Cliente")
-    telefone_cliente = int(input("Informe o Numero de Telefone do Cliente"))
-    celular_cliente = int(input("Informe o Numero de Celular do Cliente"))
-    email_cliente = input("Informe o E-mail Completo do Cliente")
+    nome_cliente = input("Informe o Nome Completo do Cliente: ")
+    cpf_cliente = int(input("Informe o CPF Completo do Cliente: "))
+    rg_cliente = int(input("Informe o  Numero de RG Completo do Cliente: "))
+    data_de_nascimento_cliente = input("Informe a Data de Nascimento do Cliente: ")
+    endereco_cliente = input("Informe o Endereço do Cliente: ")
+    cidade_cliente = input("Informe a Cidade do Cliente: ")
+    estado_cliente = input("Informe o Estado do Cliente: ")
+    telefone_cliente = int(input("Informe o Numero de Telefone do Cliente: "))
+    celular_cliente = int(input("Informe o Numero de Celular do Cliente: "))
+    email_cliente = input("Informe o E-mail Completo do Cliente: ")
 
     cliente = {
         "nome_cliente": nome_cliente,
@@ -47,18 +49,19 @@ def cadastrar_cliente(dados_cliente):
 
 
 
-def mostrar_dados_clientes():
-    for cliente in obter_dados_cliente:
+def mostrar_dados_clientes(dados_clientes):
+    for cliente in dados_clientes:
         print(f"""
-              Nome do Cliente: {cliente["nome do cliente"]}
-              CPF do Cliente: {cliente["cpf do cliente"]}
-              RG do Cliente: {cliente["RG do cliente"]}
-              Data de Nascimento do Cliente{cliente["data de nascimento do cliente"]}
-              Endereco do Cliente{cliente["endereço do cliente"]}
-              Cidade do Cliente{cliente["cidade do cliente"]}
-              Estado do Cliente{cliente["estado do cliente"]}
-              Telefone do Cliente{cliente["telefone do cliente"]}
-              Celular do Cliente{cliente["celular do cliente"]}
+              Nome do Cliente: {cliente["nome_cliente"]}
+              CPF do Cliente: {cliente["cpf_cliente"]}
+              RG do Cliente: {cliente["rg_cliente"]}
+              Data de Nascimento do Cliente: {cliente["data_de_nascimento_cliente"]}
+              Endereco do Cliente: {cliente["endereco_cliente"]}
+              Cidade do Cliente: {cliente["cidade_cliente"]}
+              Estado do Cliente: {cliente["estado_cliente"]}
+              Telefone do Cliente: {cliente["telefone_cliente"]}
+              Celular do Cliente: {cliente["celular_cliente"]}
+              email do Cliente: {cliente["email_cliente"]}
               """)
         
 def iniciar_sistema():
--- exercicio10.py
+++ exercicio10.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 cadastro = "CadastroDeFimes.json"
 
 def cadastrarFilmes():
@@ -9,10 +10,11 @@ def cadastrarFilmes():
     else:
         return[]
 
+
 def ObterCadastroFilme():
     filme = input("nome do filme: ")
-    Classificacao = input("classificação do filme: ")
-    data = input("lançado em: ")
+    Classificacao = (input("classificação do filme: "))
+    data = int(input("lançado em: "))
     tempo = input("duração do filme: ")
     descricao = input("descrição do filme: ")
 
@@ -24,22 +26,49 @@ def ObterCadastroFilme():
         "descricao": descricao
     }
 
-    return ObterCadastroFilme
+    return filmes
 
 def cadastrar_filme(dadosFilme):
     filmes = cadastrarFilmes()
     filmes.append(dadosFilme)
 
+    with open(cadastro, "w", encoding="utf-8")as arq_json:
+        json.dump(filmes, arq_json, indent=4, ensure_ascii=False)
+
+
 
 def mostrarDados_filme():
-    for filmes in ObterCadastroFilme:
+    for filmes in ObterCadastroFilme():
         print(f"""
-              nome do filme: {filmes["nome do filme"]}
-              classificacao do filme: {filmes["classificacao do filme"]}
-              data do filme: {filmes["lançamento do filme"]}
-              tempo do filme: {filmes["duração do filme"]}
-              descricao do filme: {filmes["descrição do filme"]}
+              nome do filme: {filmes["filme"]}
+              classificacao do filme: {filmes["classificacao"]}
+              data do filme: {filmes["data"]}
+              tempo do filme: {filmes["tempo"]}
+              descricao do filme: {filmes["descricao"]}
               """)
 
 
-mostrarDados_filme()
+def inciar_sistema():
+    filmes = cadastrarFilmes()
+    while True:
+        
+        print(" ")
+        print("="*80)
+        print("opção 1 - mostrar lista de filmes")
+        print("opção 2 - cadastrar filmes")
+        print("opção 3 - sair do sistema")
+        print("="*80)
+
+        opcao = input("escolha uma das opções do menu: ")
+
+        if opcao == "1" :
+            mostrarDados_filme(filmes)
+        elif opcao == "2" :
+            cadastrar_filme(ObterCadastroFilme())
+        elif opcao == "3" :
+            print("sistema finalizado.")
+        else:
+            print("opcao invalida, escolha uma das opções dos menos")
+
+inciar_sistema()
+
